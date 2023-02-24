11b906a668b95f2f3fc75d412b5619c3b11580bf
Hugo Zins <h.zins@lprs.fr>
2023-02-24 18:19:46 +0100

    fix class Task : fix Update WHERE clause and type attribute deleted

--- src/todolist/Tache.java
+++ src/todolist/Tache.java
@@ -9,37 +9,34 @@ public class Tache {
         private int id_tache;
         private String nom;
         private String description;
-        private String type_tache;
         private boolean est_realise;
 
-
-        public Tache (int id_tache, String nom, String description, String type_tache, boolean est_realise){
+        public Tache (int id_tache, String nom, String description, boolean est_realise){
             this.id_tache = id_tache;
             this.nom = nom;
             this.description = description;
-            this.type_tache = type_tache;
             this.est_realise = est_realise;
         }
+
         public void createTask(Connection maConnection) throws SQLException {
-            PreparedStatement requetePrepare = maConnection.prepareStatement("INSERT INTO tache VALUES (?,?,?,?)");
+            PreparedStatement requetePrepare = maConnection.prepareStatement("INSERT INTO tache VALUES (?,?,?)");
             requetePrepare.setString(1, this.nom);
             requetePrepare.setString(2, this.description);
-            requetePrepare.setString(3, this.type_tache);
-            requetePrepare.setBoolean(4, this.est_realise);
+            requetePrepare.setBoolean(3, this.est_realise);
             requetePrepare.executeUpdate();
         }
 
         public void updateTask(Connection maConnection) throws SQLException {
-            PreparedStatement requetePrepare = maConnection.prepareStatement("UPDATE tache SET (?,?,?,?)");
+            PreparedStatement requetePrepare = maConnection.prepareStatement("UPDATE tache SET (?,?,?) WHERE id_tache = ?");
             requetePrepare.setString(1,this.nom);
             requetePrepare.setString(2,this.description);
-            requetePrepare.setString(3, this.type_tache);
-            requetePrepare.setBoolean(4, est_realise);
+            requetePrepare.setBoolean(3, est_realise);
+            requetePrepare.setInt(4, id_tache);
             requetePrepare.executeUpdate();
         }
 
         public void deleteTask(Connection maConnection) throws SQLException{
-            PreparedStatement requetePrepare = maConnection.prepareStatement("DELETE FROM tache WHERE ?");
+            PreparedStatement requetePrepare = maConnection.prepareStatement("DELETE FROM tache WHERE id_tache = ?");
             requetePrepare.setInt(1, this.id_tache);
             requetePrepare.executeUpdate();
         }
