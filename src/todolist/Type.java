077ed0c2221a27abe81fce25ebc47d41d8f26b7a
megane <L.GBAHE-TAHO@lprs.fr>
2023-02-24 18:29:40 +0100

    create class type

--- src/todolist/Type.java
+++ src/todolist/Type.java
@@ -1,4 +1,49 @@
 package todolist;
+import java.sql.*;
+import java.util.ArrayList;
+import bdd.Bdd;
+import java.lang.Object;
 
 public class Type {
+
+    private String libelle;
+    private String codeCouleur;
+
+    public Type(String libelle, String codeCouleur) {
+
+        this.libelle = libelle;
+        this.codeCouleur = codeCouleur;
+    }
+
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public String getCodeCouleur() {
+        return codeCouleur;
+    }
+
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void setCodeCouleur(String codeCouleur) {
+        this.codeCouleur = codeCouleur;
+    }
+
+    public void typeDeTache() {
+        System.out.println("Type de tâche pour de futur filtre: " + libelle);
+        System.out.println("Code couleur : " + codeCouleur);
+    }
+
+    public void modifierType(String nouveauLibelle, String nouveauCodeCouleur) {
+        this.libelle = nouveauLibelle;
+        this.codeCouleur = nouveauCodeCouleur;
+
+    }
+
 }
+
+
