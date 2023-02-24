2f64132ca89a278525786aede851b46a22ee3f3f
Hugo Zins <h.zins@lprs.fr>
2023-02-24 16:19:15 +0100

    end class Task + fix class Bdd

--- src/todolist/Tache.java
+++ src/todolist/Tache.java
@@ -1,17 +1,18 @@
 package todolist;
 import bdd.Bdd;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Tache {
 
+        private int id_tache;
         private String nom;
         private String description;
         private boolean est_realise;
 
-        public Tache (String nom, String description, boolean est_realise){
+        public Tache (int id_tache, String nom, String description, boolean est_realise){
+            this.id_tache = id_tache;
             this.nom = nom;
             this.description = description;
             this.est_realise = est_realise;
@@ -25,21 +26,23 @@ public class Tache {
         }
 
         public void updateTask(Connection maConnection) throws SQLException {
-            PreparedStatement requetePrepare = maConnection.prepareStatement("UPDATE tache SET (nom,description,est_realise)");
+            PreparedStatement requetePrepare = maConnection.prepareStatement("UPDATE tache SET (?,?,?)");
             requetePrepare.setString(1,this.nom);
             requetePrepare.setString(2,this.description);
             requetePrepare.setBoolean(3, est_realise);
             requetePrepare.executeUpdate();
         }
 
-        public void deleteTask(Connection maConnection,) throws SQLException{
-            PreparedStatement requetePrepare = maConnection.prepareStatement("DELETE FROM tache WHERE ");
-
+        public void deleteTask(Connection maConnection) throws SQLException{
+            PreparedStatement requetePrepare = maConnection.prepareStatement("DELETE FROM tache WHERE ?");
+            requetePrepare.setInt(1, this.id_tache);
+            requetePrepare.executeUpdate();
         }
 
-        public void validTask(){
-
+        public void validTask(Connection maConnection) throws SQLException{
+            PreparedStatement requetePrepare = maConnection.prepareStatement("UPDATE tache SET est_realise = ? WHERE id_tache = ?");
+            requetePrepare.setBoolean(1, this.est_realise);
+            requetePrepare.setInt(2, this.id_tache);
+            requetePrepare.executeUpdate();
         }
-
     }
-
