65c82d77d2a6ef431c4cbc6695f00b704fa01773
Hugo Zins <h.zins@lprs.fr>
2023-02-28 12:27:40 +0100

    start main

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,9 +4,21 @@ import todolist.*;
 import utilisateur.Utilisateur;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+
+        Bdd bdd = new Bdd();
+        System.out.println("Entrez votre email : ");
+        String email = sc.nextLine();
+        System.out.println("Entrez votre mdp : ");
+        String mdp = sc.nextLine();
+
+        Utilisateur user = new Utilisateur(email, mdp, bdd);
+
+
 
 
     }
