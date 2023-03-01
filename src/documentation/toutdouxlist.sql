26b0ace55e94e4dde693848b8cf6a8a1fd698ee3
Hugo Zins <h.zins@lprs.fr>
2023-03-01 17:39:30 +0100

    fix documentation MLD and MPD

--- src/documentation/toutdouxlist.sql
+++ src/documentation/toutdouxlist.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 28 fév. 2023 à 09:36
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Généré le : mer. 01 mars 2023 à 16:37
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,19 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `liste`;
 CREATE TABLE IF NOT EXISTS `liste` (
-  `id_liste` int NOT NULL AUTO_INCREMENT,
+  `id_liste` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   PRIMARY KEY (`id_liste`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `liste`
+--
+
+INSERT INTO `liste` (`id_liste`, `nom`, `description`) VALUES
+(1, 'dfegtrhdg', 'ezrtysurutg'),
+(2, 'courses', 'manger');
 
 -- --------------------------------------------------------
 
@@ -43,11 +51,11 @@ CREATE TABLE IF NOT EXISTS `liste` (
 
 DROP TABLE IF EXISTS `participe`;
 CREATE TABLE IF NOT EXISTS `participe` (
-  `ref_utilisateur` int NOT NULL,
-  `participe.ref_liste` int NOT NULL,
-  KEY `ref_utilisateur` (`ref_utilisateur`),
-  KEY `ref_liste` (`participe.ref_liste`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ref_utilisateur` int(11) NOT NULL,
+  `ref_liste` int(11) NOT NULL,
+  PRIMARY KEY (`ref_utilisateur`,`ref_liste`),
+  KEY `fk_participe_liste` (`ref_liste`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -57,16 +65,16 @@ CREATE TABLE IF NOT EXISTS `participe` (
 
 DROP TABLE IF EXISTS `tache`;
 CREATE TABLE IF NOT EXISTS `tache` (
-  `id_tache` int NOT NULL AUTO_INCREMENT,
+  `id_tache` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `est_realise` tinyint(1) NOT NULL,
-  `tache.ref_liste` int NOT NULL,
-  `ref_type` int NOT NULL,
+  `ref_liste` int(11) NOT NULL,
+  `ref_type` int(11) NOT NULL,
   PRIMARY KEY (`id_tache`),
-  KEY `ref_type` (`ref_type`),
-  KEY `tache.ref_liste` (`tache.ref_liste`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_tache_liste` (`ref_liste`),
+  KEY `fk_tache_type` (`ref_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -76,11 +84,11 @@ CREATE TABLE IF NOT EXISTS `tache` (
 
 DROP TABLE IF EXISTS `type`;
 CREATE TABLE IF NOT EXISTS `type` (
-  `id_type` int NOT NULL AUTO_INCREMENT,
+  `id_type` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(50) NOT NULL,
   `code_couleur` varchar(50) NOT NULL,
   PRIMARY KEY (`id_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -90,13 +98,20 @@ CREATE TABLE IF NOT EXISTS `type` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `login` varchar(100) NOT NULL,
   `mdp` varchar(50) NOT NULL,
   PRIMARY KEY (`id_utilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_utilisateur`, `nom`, `prenom`, `login`, `mdp`) VALUES
+(1, 'Zins', 'Hugo', 'mail', 'mdp');
 
 --
 -- Contraintes pour les tables déchargées
@@ -106,15 +121,15 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Contraintes pour la table `participe`
 --
 ALTER TABLE `participe`
-  ADD CONSTRAINT `ref_liste` FOREIGN KEY (`participe.ref_liste`) REFERENCES `liste` (`id_liste`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ref_utilisateur` FOREIGN KEY (`ref_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_participe_liste` FOREIGN KEY (`ref_liste`) REFERENCES `liste` (`id_liste`),
+  ADD CONSTRAINT `fk_participe_utilisateur` FOREIGN KEY (`ref_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
 
 --
 -- Contraintes pour la table `tache`
 --
 ALTER TABLE `tache`
-  ADD CONSTRAINT `ref_type` FOREIGN KEY (`ref_type`) REFERENCES `type` (`id_type`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tache.ref_liste` FOREIGN KEY (`tache.ref_liste`) REFERENCES `liste` (`id_liste`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_tache_liste` FOREIGN KEY (`ref_liste`) REFERENCES `liste` (`id_liste`),
+  ADD CONSTRAINT `fk_tache_type` FOREIGN KEY (`ref_type`) REFERENCES `type` (`id_type`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
