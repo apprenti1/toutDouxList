d60620439ab561fe3b48e19e42e3b6e390ed822b
shradha <shradhakallee@gmail.com>
2023-03-02 00:23:52 +0100

    update list

--- src/todolist/Liste.java
+++ src/todolist/Liste.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Liste extends VerifFormat {
-    
-    private ArrayList<Tache>taches = new ArrayList<Tache>();
+
+    private ArrayList<Tache> taches = new ArrayList<Tache>();
     private int id_liste;
     private String nom;
     private String description;
@@ -48,29 +48,58 @@ public class Liste extends VerifFormat {
         }
     }
 
-        public void addtask (Tache taches){
-            this.taches.add(taches);
-        }
-
-        public void updateList () throws SQLException {
-            if (this.verifStringFormat(this.nom) && this.verifStringFormat(this.description)) {
-                PreparedStatement requetePrepare = this.bdd.getMaConnection().prepareStatement("UPDATE liste SET (nom = ?, description = ? WHERE id_liste = ?");
-                requetePrepare.setString(1, this.nom);
-                requetePrepare.setString(2, this.description);
-                requetePrepare.setInt(3, this.id_liste);
-                requetePrepare.executeUpdate();
-            }
-        }
-
-            public void deleteList () throws SQLException {
-                PreparedStatement requetePrepare = this.bdd.getMaConnection().prepareStatement("DELETE FROM tache WHERE id_liste=?");
-                requetePrepare.setInt(1, this.id_liste);
-                requetePrepare.executeUpdate();
-            }
-
-
-
-
+    public void addtask(Tache taches) {
+        this.taches.add(taches);
     }
+
+    public void updateList() throws SQLException {
+        if (this.verifStringFormat(this.nom) && this.verifStringFormat(this.description)) {
+            PreparedStatement requetePrepare = this.bdd.getMaConnection().prepareStatement("UPDATE liste SET (nom = ?, description = ? WHERE id_liste = ?");
+            requetePrepare.setString(1, this.nom);
+            requetePrepare.setString(2, this.description);
+            requetePrepare.setInt(3, this.id_liste);
+            requetePrepare.executeUpdate();
+        }
+    }
+
+    public void deleteList() throws SQLException {
+        PreparedStatement requetePrepare = this.bdd.getMaConnection().prepareStatement("DELETE FROM tache WHERE id_liste=?");
+        requetePrepare.setInt(1, this.id_liste);
+        requetePrepare.executeUpdate();
+    }
+
+
+    public int getId_liste() {
+        return id_liste;
+    }
+
+    public void setId_liste(int id_liste) {
+        this.id_liste = id_liste;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Bdd getBdd() {
+        return bdd;
+    }
+
+    public void setBdd(Bdd bdd) {
+        this.bdd = bdd;
+    }
+}
 
 
